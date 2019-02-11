188591ed2d2bb827d9ed8c25ac127724a6ad17b3
Anderson Jimenez <andjim2598@gmail.com>
2019-02-11 12:16:26 -0400

    [ADD] course action and list

--- models/models.py
+++ models/models.py
@@ -2,9 +2,9 @@
 
 from odoo import models, fields, api
 
- class Course(models.Model):
-     _name = 'openacademy.course'
-     _description = 'Es parte del curso de open academy'
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = 'Es parte del curso de open academy'
 
-     name = fields.Char(string='Title', required=True)
-     description = fields.Text()
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text()
