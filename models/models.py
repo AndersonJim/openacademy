3fe5120d73896fabeaef554b636573c92edf05f3
Anderson Jimenez <andjim2598@gmail.com>
2019-02-11 13:13:22 -0400

    [ADD]  session_ids to course

--- models/models.py
+++ models/models.py
@@ -9,6 +9,7 @@ class Course(models.Model):
     name = fields.Char(string='Title', required=True)
     description = fields.Text()
     resp_user_id = fields.Many2one('res.users', on_delete='set null', string='Responsible', index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
 
 class Session(models.Model):
