b267cc920cca018a330b7aa50ba8afa3c88ff86d
Anderson Jimenez <andjim2598@gmail.com>
2019-02-11 12:36:19 -0400

    [ADD] session model and views

--- models/models.py
+++ models/models.py
@@ -8,3 +8,12 @@ class Course(models.Model):
 
     name = fields.Char(string='Title', required=True)
     description = fields.Text()
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "sessiones de los cursos"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days") #por si el curso dura mediodia asi se puede representar
+    seats = fields.Integer(string="Seat's number")
