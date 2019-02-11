09bd0245d228ba8456af1c366ae799dcb68dd582
Anderson Jimenez <andjim2598@gmail.com>
2019-02-11 17:35:08 -0400

    [ADD] computed  field to session

--- models/models.py
+++ models/models.py
@@ -19,8 +19,14 @@ class Session(models.Model):
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days") #por si el curso dura mediodia asi se puede representar
-    seats = fields.Integer(string="Seat's number")
+    seats = fields.Integer(string="Seat's number", required=True)
 
     instructor_id = fields.Many2one('res.partner', string='Instructor', domain=['|', ('is_instructor', '=', True),'|',('category_id.name', 'ilike', 'Teacher'), ('category_id.parent_id.name', 'ilike', 'Teacher')])
     course_id = fields.Many2one('openacademy.course', on_delete='cascade', string='Course', required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(digits=(6, 2), compute='_taken_seats', string='Taken seats')
+
+    @api.depends('seats','attendee_ids')
+    def _taken_seats(self):
+        for record in self:
+            record.taken_seats = float(len(record.attendee_ids)/record.seats) * 100  
